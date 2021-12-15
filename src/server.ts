5053f5df9ee8f10fb57601031b00fa7c56bc7b29
claudio.silva <claudio.silva@cartesi.io>
2021-12-15 20:59:07 -0300

    Adding swagger-ui-express to document the API

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
+
 import SocketService from './service/SocketService';
 import routes from './routes';
 
@@ -22,10 +26,12 @@ SocketService.io.attach(server, {
 });
 //--
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.json());
 app.use(cors());
 app.use(routes(SocketService));
-app.set('socketio',SocketService.io); // Setting here to make avail at routes.ts
+app.set('socketio', SocketService.io); // Setting here to make avail at routes.ts
 
 // Config for http REST listening
 server.listen(SERVER_HTTP_PORT, () => console.log('Server running on port ' + SERVER_HTTP_PORT));
